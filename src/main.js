cd34f54ed4583dd091cf128af717b2605b2a065f
fmalbran93 <fmalbran93@gmail.com>
2024-06-05 16:02:24 -0300

    corrijo rutas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { __dirname } from "./path.js";
 import path from 'path';
-import handlebars from "express-handlebars";
 import { Server } from "socket.io";
 import routerProduct from "./Productos/productRoutes.js";
 import routerViews from "./common/routes/views.routes.js";
@@ -12,56 +11,50 @@ import "./config/database.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-import MongoStore from 'connect-mongo'; // Importa connect-mongo
-import mongoose from "mongoose"; // Asegúrate de importar mongoose
+import MongoStore from 'connect-mongo';
 import { createRoles } from "./config/initialSetup.js";
 import authRouter from "./Usuarios/userRoutes.js";
 import cookieRouter from "./common/routes/cookies.routes.js";
 import sessionRouter from "./common/routes/session.routes.js";
-import { engine } from 'express-handlebars';
+import exphbs from 'express-handlebars';
 
 const app = express();
 createRoles();
 const PUERTO = 8080;
 
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "src/public")));
 
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.SECRETCOOKIE));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Crea una instancia de MongoStore utilizando la conexión de mongoose
 const store = MongoStore.create({
-  mongoUrl: "mongodb+srv://fmalbran93:coderhouse@clustercoder.nqsqgsl.mongodb.net/E-commerce?retryWrites=true&w=majority&appName=ClusterCoder",
-  collection: 'sessions' // Nombre de la colección para las sesiones
+  mongoUrl: process.env.URI,
+  collection: 'sessions'
 });
 
 app.use(
-session({
-  secret: "SECRETSESSION",
-  resave: true,
-  saveUninitialized: true,
-  store: store
-})
+  session({
+    secret: process.env.SECRETSESSION,
+    resave: true,
+    saveUninitialized: true,
+    store: store
+  })
 );
- 
 
-app.engine('handlebars', engine({
+const hbs = exphbs.create({
+  layoutsDir: path.join(__dirname, 'src/common/Views/Layouts'),
+  partialsDir: path.join(__dirname, 'src/common/Views/Partials'),
   defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, 'Common/Views/Layouts'),
-  partialsDir: [
-    path.join(__dirname, 'Productos/productViews'),
-    path.join(__dirname, 'Usuarios/userViews'),
-    path.join(__dirname, 'Carritos/cartViews'),
-    path.join(__dirname, 'Mensajes/messageViews'),
-    path.join(__dirname, 'common/views/Partials')
-  ]
-}));
+  extname: '.handlebars'
+});
 
-app.set("view engine", "handlebars");
-app.set('views', path.join(__dirname, 'Views'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'src/common/Views'));
+
+// Rutas
 app.use("/api/products", routerProduct);
 app.use("/", routerViews);
 app.use("/api/carts", routerCart);
@@ -70,23 +63,17 @@ app.use("/api/cookie", cookieRouter);
 app.use("/api/session", sessionRouter);
 
 app.get("/userProfile", (req, res) => {
-  // Verifica si el usuario está autenticado
   if (req.isAuthenticated()) {
-    // Renderiza la vista userProfile.handlebars y pasa los datos del usuario como contexto
     res.render("userProfile", { user: req.user });
   } else {
-    // Si el usuario no está autenticado, redirige a la página de inicio de sesión
     res.redirect("/login");
   }
 });
 
 const httpServer = app.listen(PUERTO, () => {
-    console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+  console.log(`Listening on the port http://localhost:${PUERTO}`);
 });
 
-
-const socketServer = new Server(httpServer); // Creamos una nueva instancia de 'Server' con 'httpServer'
-
+const socketServer = new Server(httpServer);
 socketProducts(socketServer);
 socketChat(socketServer);
-
